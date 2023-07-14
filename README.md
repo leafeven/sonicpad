1848ee813e7054964d55bfb8fcf283b59f1d6e4e
leafkattree <leaf@kattree.org>
2023-07-13 19:39:23 -0500

    intial code, example config, update README.md

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # sonicpad
-Sonic pad scripts
+Sonic pad scripts, well currently just the one script, the purpose of which is to allow sending pushover notifications from the Creality Sonic Pad. If some time in the future Creality adds apprise or some enterprising individual figures out how to install it, this script is obsolete as moonraker will be able to do everything this can without having to root and appears to have better hooks in to what is going on in the printer. 
+
+### Prerequisites 
+First you will need to be able to login via ssh as root into your Sonic Pad. Creality provides an easy way to get the root credentials now from Sonic Pad > Configure > Other Settings > Advanced options > Root Account. You will need to agree to the root notice. Understanding you can mess up your Sonic Pad if you are not careful, **do at your own risk**.
+
+### Installation
+If you are choosing to continue, you can either ssh in to your sonic pad with something like putty or use WinSCP to copy the files to your sonic pad, choice is yours. 
+#### Putty
+1. ssh into your Sonic Pad then `cd /user/share/klipper/klippy/extras`
+2. using vi make the file `vi gcode_pushover.py. press` "I" then paste in the text (right click in putty), then press esc, then type ":wq" to save and close the file.
+3. restart the Sonic Pad.
+
+### Printer.cfg 
+Backup your printer.cfg file just to be safe and add the macros in the example_printer.cfg, making sure to update the user token and app token with your tokens from pushover. if you don’t have a webcam attached to your Sonic pad remove the URL in the attach line. Update any of the SEND_PUSHOVER_MESSAGE lines, making sure the title and message strings are surrounded by double quotes ("")
+Save printer.cfg and restart klipper. You should now be able to go to the console and type "PUSH_TEST"
+
+### Light Reading
+The original inspiration for this is the gcode_shell_command.py written by Eric Callahan. 
+[Original Post the led me down this rabbit hole]( https://www.teamfdm.com/forums/topic/816-howto-pushover-notifications-for-completed-prints-to-your-phone/)
+[More information on Eric’s gcode_shell_command.py]( https://github.com/th33xitus/kiauh/blob/master/docs/gcode_shell_command.md)
---
+++ gcode_pushover.py
@@ -0,0 +1,79 @@
+###############################################################################
+# Name: gcode_pushover.py
+# Desc: send messages via gcode to pushover
+# Note: requests and apprise libraries missing from sonic pad, this prevents 
+#       using moonraker notifier, and is why urllib3 is used instead
+#       Save to /usr/share/klipper/klippy/extras on sonic pad
+#
+# Note: bits of code taken directly from Eric Callahan's gcode_shell_command.py
+### Imports ###################################################################
+from urllib3 import PoolManager
+from io import BytesIO
+import logging
+
+### Class #####################################################################
+class PushoverNotifier:
+    URL = 'https://api.pushover.net/1/messages.json'
+    HELP = 'send messages via gcode to pushover'
+
+    def __init__(self,config):
+        self.name = config.get_name().split()[-1]
+        self.printer = config.get_printer()
+        self.gcode = self.printer.lookup_object('gcode')
+
+        self.user_token = config.get('user')
+        self.app_token = config.get('token')
+        self.attachment = config.get('attach','')
+        self.verbose = config.getboolean('verbose', True)
+
+        self.gcode.register_mux_command(
+            'SEND_PUSHOVER_MESSAGE','CMD',self.name,
+            self.send_message,
+            desc=self.HELP
+        )
+    
+    def send_message(self,params):
+        '''Send message to pushover'''
+        title = params.get('TITLE','Test Message')
+        message = params.get('MESSAGE','This is a test')
+        priority = params.get('PRIORITY','0')
+
+        try:
+            http = PoolManager()
+
+            fields = {
+                'token':self.app_token,
+                'user':self.user_token,
+                'priority':priority,
+                'title':title,
+                'message':message,
+            }
+
+            if self.attachment:
+                pic = BytesIO()
+                request = http.request('GET',self.attachment,preload_content=False)
+                while True:
+                    data = request.read(1000)
+                    if not data:
+                        break
+                    pic.write(data)
+                
+                if pic.getbuffer().nbytes > 0:
+                    fields['attachment'] = ('snapshot.jpg',pic.getbuffer(),'image/jpeg')
+                request.release_conn()
+            
+            response = http.request('POST',self.URL,fields=fields)
+            if self.verbose:
+                self.gcode.respond_info('Response Status: {}'.format(response.status))
+                self.gcode.respond_info('Response: {}'.format(response.data.decode()))
+
+            response.release_conn()
+        
+        
+        except Exception:
+            logging.exception('pushover_message: {%s} failed' % (self.name))
+            raise self.gcode.error("Error sending pushover message {%s}" % (self.name))
+
+
+def load_config_prefix(config):
+    return PushoverNotifier(config)
